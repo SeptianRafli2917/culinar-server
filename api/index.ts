2031b86b34b3b0053b799cbfa30b290a6a0798ec
SeptianRafli2917 <septianraflisyahputra@gmail.com>
2025-06-10 16:24:05 +0700

    deploy

--- api/index.ts
+++ api/index.ts
@@ -1,23 +1,8 @@
-import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "../routes";
-import serverless from 'serverless-http';
-
+const express = require("express");
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
-registerRoutes(app);
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.get("/", (_req: Request, res: Response) => {
-  res.json({
-    message: "Welcome to the API! Use /api for API routes.",
-    status: "ok",
-  });
-});
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
-});
-
-export default module.exports = app;
+module.exports = app;
